b595a3139a2cb474bbbe3fe02bff5bd3f79c5ffa
spaliwal <spaliwal@adobe.com>
2019-10-11 13:02:17 +0530

    Integrated Logging lib with target sdk Renamed repo to aio-lib-target

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ governing permissions and limitations under the License.
 
 const Swagger = require('swagger-client')
 const { codes } = require('./SDKErrors')
-const debugNamespace = 'aio-cna-core-target'
-const debug = require('debug')(debugNamespace)
+const logger = require('@adobe/aio-lib-core-logging')('aio-lib-target', { level: process.env.LOG_LEVEL })
 
 function init (tenant, apiKey, token) {
   return new Promise((resolve, reject) => {
@@ -22,11 +21,11 @@ function init (tenant, apiKey, token) {
 
     clientWrapper.init(tenant, apiKey, token)
       .then(initializedSDK => {
-        debug('sdk initialized successfully')
+        logger.debug('sdk initialized successfully')
         resolve(initializedSDK)
       })
       .catch(err => {
-        debug(`sdk init error: ${err}`)
+        logger.debug(`sdk init error: ${err}`)
         reject(err)
       })
   })
